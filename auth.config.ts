285c5019649769529f2a570450daba632d488ff6
A <155712261+EJL000@users.noreply.github.com>
2024-01-07 23:49:08 +0800

    Version 6

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,12 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnHome = nextUrl.pathname.startsWith('');
+      const isOnHome = nextUrl.pathname.startsWith('/home/index');
       if (isOnHome) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('', nextUrl));
+        return Response.redirect(new URL('/home/index', nextUrl));
       }
       return true;
     },
