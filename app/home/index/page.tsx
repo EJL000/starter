cc1fd20eb4aa95045304ce18de1b906007549b54
EJL000 <155712261+EJL000@users.noreply.github.com>
2024-01-08 10:35:12 +0800

    Update page.tsx

--- app/home/index/page.tsx
+++ app/home/index/page.tsx
@@ -65,14 +65,6 @@ export default async function Page() {
       <h2>Here is the list of territories</h2>
       <div dangerouslySetInnerHTML={{__html: test}}>
       </div>
-      <form action={async () => {
-            'use server';
-            await signOut();
-          }}>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
     </div>
   );
 }
